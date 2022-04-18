ce1c0f4632000fcb8a037949a3257be7b8950915
Nikita <oficialsmashgame@gmail.com>
2022-04-18 19:59:55 +0300

    Updates

---
+++ precache-manifest.159e00e6a652208805316478ade6160f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f0cf812cc6c70e0e95a",
+    "url": "/React_GOTapp/static/css/main.c1f7c265.chunk.css"
+  },
+  {
+    "revision": "6f0cf812cc6c70e0e95a",
+    "url": "/React_GOTapp/static/js/main.6f0cf812.chunk.js"
+  },
+  {
+    "revision": "8dcdde52584776b32ddf",
+    "url": "/React_GOTapp/static/css/1.4bdf8844.chunk.css"
+  },
+  {
+    "revision": "8dcdde52584776b32ddf",
+    "url": "/React_GOTapp/static/js/1.8dcdde52.chunk.js"
+  },
+  {
+    "revision": "0d1077bc54188d63b6cf",
+    "url": "/React_GOTapp/static/js/runtime~main.0d1077bc.js"
+  },
+  {
+    "revision": "13d692d639c009fc89cb36efc76e5253",
+    "url": "/React_GOTapp/index.html"
+  }
+];
